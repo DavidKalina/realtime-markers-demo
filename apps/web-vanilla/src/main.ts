db7cfd86324560b3531cbfd4b95aa38baeee8ca9
Mario Oately <davidkalina@proton.me>
2025-02-25 20:19:31 -0700

    feat: lean up stack

--- apps/web-vanilla/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { MarkerGraph } from "./MarkerGraph";
-
-// Initialize the graph when the DOM is loaded
-document.addEventListener("DOMContentLoaded", () => {
-  new MarkerGraph("graph-container");
-});
