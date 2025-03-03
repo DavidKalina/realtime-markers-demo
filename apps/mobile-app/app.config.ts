7d9c66e7b0ec093871ed377af274805d59faebb1
Mario Oately <davidkalina@proton.me>
2025-03-02 20:37:22 -0700

    bump

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.0.6",
+    version: "1.0.7",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
