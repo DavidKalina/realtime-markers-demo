e8c523fc76cd2b338f6b41906de138de95db3006
Mario Oately <davidkalina@proton.me>
2025-03-03 20:14:38 -0700

    feat: enable OTA updates

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.1.3",
+    version: "1.1.4",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -31,7 +31,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "14",
+      buildNumber: "15",
     },
     android: {
       adaptiveIcon: {
