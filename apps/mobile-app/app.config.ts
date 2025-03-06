d48dd79c652ee3eaeaef138f78bbfbe9970a78b5
Mario Oately <davidkalina@proton.me>
2025-03-06 08:13:50 -0700

    bump

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.1.7",
+    version: "1.1.8",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -31,7 +31,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "18",
+      buildNumber: "19",
     },
     android: {
       adaptiveIcon: {
