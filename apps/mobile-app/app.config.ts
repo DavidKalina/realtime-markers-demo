16464c1523489f11f435e8c62d2eb36446fa0685
Mario Oately <davidkalina@proton.me>
2025-03-15 18:00:59 -0600

    feat: update camera controls

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.3.7",
+    version: "1.3.8",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -31,7 +31,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "38",
+      buildNumber: "39",
     },
     android: {
       adaptiveIcon: {
