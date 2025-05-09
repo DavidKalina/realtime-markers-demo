2b52cad3803f0ec1b794c553e80a71374ae4e5f5
David Kalina <davidkalina@proton.me>
2025-05-09 11:33:48 -0600

    chore: update version to 1.12.5 and increment build number to 126

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.12.4",
+    version: "1.12.5",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "125",
+      buildNumber: "126",
     },
     android: {
       adaptiveIcon: {
