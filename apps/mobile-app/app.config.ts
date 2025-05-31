cf114278e5443545154705e8075474aa4827d9d8
David Kalina <davidkalina@proton.me>
2025-05-31 07:28:47 -0600

    chore: update app version and build number for mobile app
    - Bumped version from 1.13.1 to 1.13.2 to reflect new release.
    - Incremented build number from 132 to 133 for consistency with versioning.

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.13.1",
+    version: "1.13.2",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "132",
+      buildNumber: "133",
     },
     android: {
       adaptiveIcon: {
