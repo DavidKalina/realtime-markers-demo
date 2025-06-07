8b26cd694596f96e93dd7ae39f8b00f2d7fc095b
David Kalina <davidkalina@proton.me>
2025-06-07 11:11:15 -0600

    chore: increment app version and build number to 1.13.9 and 140 respectively
    - Updated the app version to 1.13.9 in preparation for the next release.
    - Incremented the build number to 140 to reflect recent changes.

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.13.8",
+    version: "1.13.9",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "139",
+      buildNumber: "140",
     },
     android: {
       adaptiveIcon: {
