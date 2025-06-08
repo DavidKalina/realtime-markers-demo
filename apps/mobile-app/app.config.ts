28d51bc2b7e2fadf2a831eb656f02ded1cbdd858
David Kalina <davidkalina@proton.me>
2025-06-08 12:09:23 -0600

    chore: bump app version and build number for mobile app
    - Updated the app version from 1.14.1 to 1.14.2 to reflect recent changes.
    - Incremented the build number from 142 to 143 for the latest release.

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "MapMoji",
     slug: "mobile-app",
-    version: "1.14.1",
+    version: "1.14.2",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "142",
+      buildNumber: "143",
     },
     android: {
       adaptiveIcon: {
