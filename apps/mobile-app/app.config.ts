6d225330091fa16d5160e9a5708a4c017d4cb8d2
David Kalina <davidkalina@proton.me>
2025-06-23 23:23:47 -0600

    chore: update app version and build number for mobile app
    - Bumped app version from 1.15.9 to 1.16.0 for the mobile application.
    - Incremented build number from 160 to 161 to reflect the latest changes.

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Frederick",
     slug: "mobile-app",
-    version: "1.15.9",
+    version: "1.16.0",
     orientation: "portrait",
     icon: "./assets/images/frederick-logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "160",
+      buildNumber: "161",
     },
     android: {
       adaptiveIcon: {
