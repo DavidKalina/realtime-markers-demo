0d1b0990274e7e592d587d03900482657aa5eb29
David Kalina <davidkalina@proton.me>
2025-06-23 18:54:23 -0600

    chore: bump version and build number to 1.15.9 and 160 respectively

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Frederick",
     slug: "mobile-app",
-    version: "1.15.8",
+    version: "1.15.9",
     orientation: "portrait",
     icon: "./assets/images/frederick-logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "159",
+      buildNumber: "160",
     },
     android: {
       adaptiveIcon: {
