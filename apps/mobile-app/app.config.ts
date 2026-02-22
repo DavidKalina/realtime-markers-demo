c4ff9f59c16098191175e6937044cdde6904af64
David Kalina <davidkalina@proton.me>
2026-02-22 10:15:10 -0700

    chore: update version to 1.17.2 and increment build number to 173 in app.config.ts

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Realtime Markers",
     slug: "mobile-app",
-    version: "1.17.1",
+    version: "1.17.2",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -30,7 +30,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "172",
+      buildNumber: "173",
     },
     android: {
       adaptiveIcon: {
