8df8433319583409ea3dcaf24c036bbe4c07b8d7
David Kalina <davidkalina@proton.me>
2026-02-26 22:31:46 -0700

    chore: update app version and build number to 1.18.0 and 181 respectively

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Realtime Markers",
     slug: "mobile-app",
-    version: "1.17.9",
+    version: "1.18.0",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -30,7 +30,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "180",
+      buildNumber: "181",
     },
     android: {
       adaptiveIcon: {
