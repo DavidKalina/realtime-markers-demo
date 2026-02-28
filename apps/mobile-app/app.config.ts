e3246a183a2631228ed203b70a364338cf9cfcda
David Kalina <davidkalina@proton.me>
2026-02-28 11:23:52 -0700

    fix: improve deployment process and update mobile app version
    - Enhanced the deployment workflow to wait for PostgreSQL readiness before running migrations, ensuring a smoother deployment process.
    - Updated mobile app version to 1.18.4 and build number to 185, reflecting recent changes and improvements.

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Realtime Markers",
     slug: "mobile-app",
-    version: "1.18.3",
+    version: "1.18.4",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -30,7 +30,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "184",
+      buildNumber: "185",
     },
     android: {
       adaptiveIcon: {
