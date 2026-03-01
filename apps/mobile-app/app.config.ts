9a43954fa129a5699aff7284ed8c86be898bfc31
David Kalina <davidkalina@proton.me>
2026-02-28 23:41:40 -0700

    chore: update mobile app version to 1.18.9 and build number to 190

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Realtime Markers",
     slug: "mobile-app",
-    version: "1.18.8",
+    version: "1.18.9",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "189",
+      buildNumber: "190",
     },
     android: {
       adaptiveIcon: {
