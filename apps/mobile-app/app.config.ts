b33ba4c502f7762437193676ce0f7d47f07aa032
David Kalina <davidkalina@proton.me>
2026-03-02 14:25:28 -0700

    bump

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Realtime Markers",
     slug: "mobile-app",
-    version: "1.19.1",
+    version: "1.19.2",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "192",
+      buildNumber: "193",
     },
     android: {
       adaptiveIcon: {
