331ccae43103a970cd0e08c1ddb70115ff335421
David Kalina <davidkalina@proton.me>
2026-03-04 14:34:03 -0700

    chore: update app version to 1.20.0 and build number to 201

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "A Third Space",
     slug: "mobile-app",
-    version: "1.19.9",
+    version: "1.20.0",
     orientation: "portrait",
     icon: "./assets/app-icon.png",
     scheme: "myapp",
@@ -35,7 +35,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "200",
+      buildNumber: "201",
     },
     android: {
       label: "A Third Space",
