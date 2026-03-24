15e17bbe7786dd2562ad57e8b4b956c3b25e91b9
David Kalina <davidkalina@proton.me>
2026-03-24 15:57:07 -0600

    chore: update mobile app version to 1.25.0 and build number to 251

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Sidequests",
     slug: "mobile-app",
-    version: "1.24.9",
+    version: "1.25.0",
     orientation: "portrait",
     icon: "./assets/app-icon.png",
     scheme: "myapp",
@@ -44,7 +44,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "250",
+      buildNumber: "251",
     },
     android: {
       label: "Sidequests",
