061bda1c31b3832e61868bb189b02741005dd6e9
David Kalina <davidkalina@proton.me>
2026-04-03 21:15:33 -0600

    bump

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: "Sidequests",
     slug: "mobile-app",
-    version: "2.2.1",
+    version: "2.2.2",
     orientation: "portrait",
     icon: "./assets/app-icon-deck.png",
     scheme: "myapp",
@@ -44,7 +44,7 @@ export default {
       },
       supportsTablet: true,
       bundleIdentifier: "com.tenuto.mobileapp",
-      buildNumber: "280",
+      buildNumber: "281",
     },
     android: {
       label: "Sidequests",
